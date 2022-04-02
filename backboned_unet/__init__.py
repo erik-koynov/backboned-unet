e8d0e29c139041b2c292a3cb15fae6826ba44408
lexhd <lexhd@rtx2070.lexoro.ai>
2022-04-02 21:56:46 +0200

    set logging to warning

--- backboned_unet/__init__.py
+++ backboned_unet/__init__.py
@@ -2,7 +2,7 @@ from .unet import Unet
 from .metrics import iou, soft_iou, dice_score, DiceLoss
 import logging
 logger = logging.getLogger('backboned_unet')
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.WARNING)
 handler = logging.StreamHandler()
 handler.setFormatter(logging.Formatter('[%(name)s: %(filename)s: %(funcName)s]- %(levelname)s - %(message)s'))
 logger.addHandler(handler)
